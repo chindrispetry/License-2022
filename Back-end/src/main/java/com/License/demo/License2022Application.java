5beb76f5681c20a6bdf975ab2714e5d77ec9ef52
Chindris <chindrispetry@yahoo.com>
2022-05-17 09:17:50 +0300

    Start back-end

---
+++ Back-end/src/main/java/com/License/demo/License2022Application.java
@@ -0,0 +1,13 @@
+package com.License.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class License2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(License2022Application.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/License/demo/License2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.License.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class License2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
